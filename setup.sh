8daa9b9074eaec884264171ff5e0fead35c690a5
prince3103 <prince.sethi@trantorinc.com>
2021-07-23 11:04:56 +0530

    added __init__.py

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo $"#/bin/sh\nblack .\npython lint.py -p ../CalculatorLibrary-master/"> .git/hooks/pre-commit
+echo $'#/bin/sh\nblack .\npython lint.py -p ../cicd_ci_circle/'> .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
