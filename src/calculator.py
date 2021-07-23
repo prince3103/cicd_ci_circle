842278fa3f08f20526b900461a926ed7f509316e
prince3103 <prince.sethi@trantorinc.com>
2021-07-23 16:34:30 +0530

    ds

--- src/calculator.py
+++ src/calculator.py
@@ -12,5 +12,5 @@ def subtract(first_term, second_term):
 
 
 def multiply(first_term, second_term):
-    return first_term * second_term
-    print("clone_offli")
+    return first_term
+    print("clone_offl")
