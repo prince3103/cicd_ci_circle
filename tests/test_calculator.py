3b0facb5b20ef47f7a6ba9823691b50d7f1e3e7f
prince3103 <prince.sethi@trantorinc.com>
2021-07-23 16:26:30 +0530

    ds

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -17,7 +17,7 @@ class TestCalculator:
         assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(6, 2)
+        assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
