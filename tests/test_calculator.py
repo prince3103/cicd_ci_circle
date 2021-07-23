a73bc5c6d8da2edccb07d85a6fcf2c21d4e4e895
prince3103 <prince.sethi@trantorinc.com>
2021-07-23 16:22:05 +0530

    d

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,7 +7,7 @@ import inspect
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
+sys.path.insert(0, parentdir)
 
 from src import calculator
 
@@ -15,9 +15,9 @@ from src import calculator
 class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-    
+
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        assert 4 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
